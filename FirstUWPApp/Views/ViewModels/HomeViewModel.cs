d733efd37aa9090382376e4690913566cd01d930
Artem <artyom543211@gmail.com>
2019-02-07 21:19:05 +0300

    Fix bag in style.xaml

--- FirstUWPApp/Views/ViewModels/HomeViewModel.cs
+++ FirstUWPApp/Views/ViewModels/HomeViewModel.cs
@@ -7,9 +7,20 @@ using System.Threading.Tasks;
 
 namespace FirstUWPApp.Views.ViewModels
 {
-    public class HomeViewModel : AbstractViewModel
-    {
-        public HomeViewModel()
+	public class HomeViewModel : AbstractViewModel
+	{
+		private string _syncText;
+		public string SyncText
+		{
+			get { return _syncText; }
+			set
+			{
+				_syncText = value;
+				OnPropertyChanged(nameof(SyncText));
+			}
+		}
+
+		public HomeViewModel()
         {
             MainText = "Welcome to Home page";//(string)App.Current.Resources["TitleHomePage"];
         }
